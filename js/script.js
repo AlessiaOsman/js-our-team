e1268142cbc498bfd04fcf723256063cb8025552
AlessiaOsman <alessia.osman@gmail.com>
2024-01-15 17:02:21 +0100

    added card

--- js/script.js
+++ js/script.js
@@ -34,11 +34,43 @@ const team = [
 
 ]
 
+// Recupero elementi in pagina
+
+const containerElement = document.getElementById('cards-container')
+
+let card = '';
+
+
 //Stampare su console le informazioni di nome, ruolo e la stringa della foto
 
-for (let i = 0; i < team.length; i++){
-    console.log('Nome: ' + team[i]['name']);
-    console.log('Ruolo: ' + team[i]['role']);
-    console.log('Foto: ' + team[i]['pic'])
-    console.log('-------------------------------')
+for (let i = 0; i < team.length; i++) {
+
+    let cardName = team[i]['name']
+    let cardRole = team[i]['role']
+    let cardPic = team[i]['pic']
+
+    // Creata card
+    
+    card += `
+    <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./img/${cardPic}" class="img-fluid rounded-start">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${cardName}</h5>
+              <p class="card-text">${cardRole}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    `
+    
 }
+
+containerElement.innerHTML = card
+
+
+
